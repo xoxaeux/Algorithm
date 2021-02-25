de053ce73a58e8173ec19077897ad9302763bdb6
xoxaeux <striplehs@ajou.ac.kr>
2021-02-26 00:19:39 +0900

    BOJ_2564

--- BOJ/Main2564.java
+++ BOJ/Main2564.java
@@ -25,21 +25,19 @@ public class Main2564 {
 		int cnt = 0;
 		for (int i = 0; i < N; i++) {
 			cnt += sol(col, row, x, y, points[i][0], points[i][1]);
-			System.out.println(cnt);
 		}
 		System.out.println(cnt);
 	}
 
 //	방향이 서로 같다면, 차이만 구하면 된다.
-//	다를 경우는 총 3가지 경우. 차이가 2라면, 서로의 위치값 +다른 선분길이
+//	다를 경우는 총 3가지 경우. 서로 마주보는 경우, 서로의 위치값 +다른 선분길이
 //	차이가 1일 경우, 북서/남동/북동/서남으로 나누어서 계산해보자.
 //	그리고 나머지 경우는 총 길이에서 뺀 값이므로 비교해볼수 있다.
 	private static int sol(int X, int Y, int x, int y, int i, int j) {
 		int num;
 		if (x == i) {
 			num = Math.abs(y - j);
-			System.out.println("1"+num);
-		} else if (Math.abs(x - i) == 2) {
+		} else if ((x == 1 && i == 2) || (x == 2 && i == 1) || (x == 3 && i == 4) || (x == 4 && i == 3)) {
 			if (x == 1 || x == 2) {
 				num = y + j + Y;
 			} else {
@@ -50,13 +48,18 @@ public class Main2564 {
 				num = y + j;
 			} else if (x + i == 6) {
 				num = (y + j + X + Y);
-			} else if (x == 1 || x == 4) {
-				num = X - y + j;
 			} else {
-				num = y + Y - j;
+				if (x == 1)
+					num = X - y + j;
+				else if (x == 4)
+					num = X - j + y;
+				else if (x == 2)
+					num = Y - j + y;
+				else
+					num = Y - y + j;
 			}
 		}
-		return Math.min(num, 2*X+2*Y-num);
+		return Math.min(num, 2 * X + 2 * Y - num);
 	}
 
 }
