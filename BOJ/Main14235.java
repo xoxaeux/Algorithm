fde15ba299073b5d6bdd57832d3e05b8fb8e5502
Do Taeuk <69946102+xoxaeux@users.noreply.github.com>
2021-06-10 22:25:36 +0900

    BOJ_14235

--- BOJ/Main14235.java
+++ BOJ/Main14235.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.*;
 public class Main {
-		public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
